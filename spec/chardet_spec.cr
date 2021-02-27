72d0c85c7543e312fd1db25d009c726ecdd6cbd2
Eddie Lebow <elebow@users.noreply.github.com>
2021-02-27 01:37:45 -0500

    Write a real test

--- spec/chardet_spec.cr
+++ spec/chardet_spec.cr
@@ -3,7 +3,7 @@ require "../src/chardet"
 require "./spec_helper"
 
 describe Chardet do
-  it "TODO" do # TODO temporary test driver
-    puts Chardet.detect("aaa")
+  it "detects an encoding" do
+    Chardet.detect("♥".encode("utf-8")).should eq({encoding: "UTF-8"})
   end
 end
